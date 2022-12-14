b2a6077bdb4251b763615a887196384fc3b1f3d5
owenramber1989 <owenramber1989@163.com>
2022-12-14 19:03:19 +0800

    FINISHED lab01

--- lab01/exercise3/ll_cycle.c
+++ lab01/exercise3/ll_cycle.c
@@ -1,6 +1,18 @@
-#include <stddef.h>
 #include "ll_cycle.h"
+#include <stddef.h>
 
 int ll_has_cycle(node *head) {
-    /* TODO: Implement ll_has_cycle */
+  /* TODO: Implement ll_has_cycle */
+  node *fast_ptr = head;
+  node *slow_ptr = head;
+  while (fast_ptr) {
+    fast_ptr = fast_ptr->next;
+    fast_ptr = fast_ptr->next;
+    slow_ptr = slow_ptr->next;
+    if (!fast_ptr)
+      return 0;
+    if (fast_ptr == slow_ptr)
+      return 1;
+  }
+  return 0;
 }
