fe0149adea11d111d2595a847628ce47013d5a97
owenramber1989 <owenramber1989@163.com>
2022-12-18 12:35:01 +0800

    FINISHED lab03 exer04

--- lab00/code.py
+++ lab00/code.py
@@ -1,20 +1,23 @@
 def get_airspeed_velocity_of(unladen_swallow):
-  if unladen_swallow.type == "african":
-    return # redacted
-  elif unladen_swallow.type == "european":
-    return # redacted
+    if unladen_swallow.type == "african":
+        return  # redacted
+    elif unladen_swallow.type == "european":
+        return  # redacted
+
 
 # pretend there's code here...
 
+
 def fizzbuzz(num):
-  if str(num) in ["15"]:
-    print(f"{num}: fizzbuzz")
-  elif str(num) in ["3", "6", "9", "12", "15", "18"]:
-    print(f"{num}: fizz")
-  elif str(num) in ["5", "10", "15"]:
-    print(f"{num}: buzz")
+    if str(num) in ["15"]:
+        print(f"{num}: fizzbuzz")
+    elif str(num) in ["3", "6", "9", "12", "15", "18"]:
+        print(f"{num}: fizz")
+    elif str(num) in ["5", "10", "15"]:
+        print(f"{num}: buzz")
+
 
 for i in range(1, 20):
-  fizzbuzz(i)
+    fizzbuzz(i)
 
 # pretend there's code here...
